1a1947fda7e03d9cba25d236b136ab05f6be0edf
a.pesterev <a.pesterev@council.ru>
2022-12-22 20:33:27 +0300

    Initial

--- check.py
+++ check.py
@@ -1,3 +1,8 @@
+import glob
+import contextlib
+
+from PIL import Image
+
 import json
 import os.path as path
 
@@ -6,20 +11,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import DataFrame
 
+import decimal
+
 CONFIGURATION_FILE_PATH: str = "C:/Users/a.pesterev/CLionProjects/porous_media/configuration.json"
-RESULT_FOLDER_PATH: str = "C:/Users/a.pesterev/CLionProjects/porous_media/cmake-build-release/results"
+RESULT_FOLDER_PATH: str = r"C:\Users\a.pesterev\CLionProjects\porous_media\cmake-build-release-visual-studio\results"
 
 assert path.exists(CONFIGURATION_FILE_PATH), f"Configuration file path does not exists! {CONFIGURATION_FILE_PATH}"
 
 with open(CONFIGURATION_FILE_PATH) as configuration_file:
-    CONFIGURATION: object = json.load(configuration_file)
+    CONFIGURATION: dict = json.load(configuration_file)
 
 RESULT_FILE: str = path.join(RESULT_FOLDER_PATH, "result.csv")
 
 df: DataFrame = pd.read_csv(RESULT_FILE, sep=';', skiprows=4, index_col=False,
                             dtype={'c': np.float64, 's': np.float64, 'x': np.float64, 't': np.float64})
-
-print(df)
 
 ANALYTIC_SOLUTION_IS_KNOWN = False
 
@@ -32,12 +37,52 @@ def plot_with_condition(passed_df: pd.DataFrame, condition_instance: object, **k
     :type condition_instance: object
     """
     fixed_x_df = passed_df[condition_instance]
-    print(fixed_x_df)
+    # print(fixed_x_df)
     fixed_x_df.plot(**kwargs)
 
 
-plotted_xs: list[int | float] = np.arange(0, 0.25, 0.01)
+def make_gif_from_pngs(png_files_regex: str | list[str], output_file: str):
+    # use exit stack to automatically close opened images
+    with contextlib.ExitStack() as stack:
+        # lazily load images
+        if png_files_regex is str:
+            imgs = (stack.enter_context(Image.open(f))
+                    for f in glob.glob(png_files_regex))
+        elif isinstance(png_files_regex, list):
+            imgs = (stack.enter_context(Image.open(f))
+                    for f in png_files_regex)
+        else:
+            raise ValueError(f"Unexpected type of param {png_files_regex}, {type(png_files_regex)}!")
+
+        # extract  first image from iterator
+        img = next(imgs)
+
+        # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+        img.save(fp=output_file, format='GIF', append_images=imgs,
+                 save_all=True, duration=200, loop=0)
+
+
+H: float = CONFIGURATION['h']
+DECIMAL_PLACES = abs(decimal.Decimal(str(H)).as_tuple().exponent)
+
+H = 0.05
+MAX_X_TO_PLOT = 1
+
+plotted_xs: list[int | float] = list(map(
+    lambda el: float((('%.' + str(DECIMAL_PLACES) + 'f') % round(el, DECIMAL_PLACES))),
+    np.arange(0, MAX_X_TO_PLOT + H, H)
+))
+
+print(plotted_xs)
+
 subplot_numbers: list[int] = [111 for _ in range(len(plotted_xs))]
+
+C_COORDINATE = 'c(i, j)'
+
+print(C_COORDINATE in df.columns)
+print(df[df[f"{C_COORDINATE}"] < 0])
+
+output_filenames: list[str] = list()
 
 plotted_x: int | float
 subplot_number: int
@@ -51,7 +96,7 @@ for plotted_x, subplot_number in zip(plotted_xs, subplot_numbers):
         ax=ax,
         xlabel="Время, t"
     )
-    plot_with_condition(df, condition, label='Вычисленная концентрация', y="c", linestyle='-', **args)
+    plot_with_condition(df, condition, label='Вычисленная концентрация', y=C_COORDINATE, linestyle='-', **args)
     if ANALYTIC_SOLUTION_IS_KNOWN:
         plot_with_condition(df, condition, y="c_e", label='Аналитическое решение для концентрации', linestyle='-.',
                             **args)
@@ -59,7 +104,13 @@ for plotted_x, subplot_number in zip(plotted_xs, subplot_numbers):
     if ANALYTIC_SOLUTION_IS_KNOWN:
         plot_with_condition(df, condition, y="s_e", label='Аналитическое решение для осадка', linestyle='-.', **args)
     plt.legend()
-    plt.show()
+    filename: str = f'x={int(plotted_x / H)}.png'
+    output_filenames.append(filename)
+    plt.savefig(filename)
+    ax.cla()
+    plt.clf()
+
+make_gif_from_pngs(output_filenames, 'output.gif')
 
 # plotted_ts: list[int | float] = [0.5, 1, 2, 5, 10, 20, 50, 100]
 #
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <cmath>
 #include <Eigen/Sparse>
+#include <gmp.h>
 
 #define out(mes, val) std::cout << "\t" << mes << ": " << val << '\n';
 
@@ -14,50 +15,89 @@
 #define IS_FLOAT
 
 #ifndef IS_FLOAT
-#define TYPE double
+#ifndef IS_DOUBLE
+#ifndef IS_LONG_DOUBLE
+#define MPZ_T
+#include <string>
+typedef mpz_t INTEGER_TYPE;
+typedef mpf_t FLOAT_TYPE;
+typedef std::string CONTAINER_TYPE;
 #else
-#define TYPE float
+typedef long long INTEGER_TYPE;
+typedef long double FLOAT_TYPE;
+typedef long double CONTAINER_TYPE;
+#endif
+#else
+typedef long long INTEGER_TYPE;
+typedef double FLOAT_TYPE;
+typedef double CONTAINER_TYPE;
+#endif
+#else
+typedef long long INTEGER_TYPE;
+typedef float FLOAT_TYPE;
+typedef float CONTAINER_TYPE;
 #endif
 
+#ifndef MPZ_T
+const FLOAT_TYPE eps = FLOAT_TYPE(1e-5);
+const FLOAT_TYPE one = FLOAT_TYPE(1);
+const FLOAT_TYPE zero = FLOAT_TYPE(0);
 
-const TYPE eps = TYPE(1e-5);
-
-
-TYPE v(TYPE t, const std::function<TYPE(TYPE, TYPE, TYPE)> &f_s_0, TYPE s_m, TYPE delta_0) {
-    return TYPE(1) - (f_s_0(t, delta_0, s_m) / s_m);
-}
-
-TYPE s_0(TYPE t, TYPE delta_0, TYPE s_m) {
-    return s_m * (TYPE(1) - exp((TYPE(-1) * t * delta_0) / s_m));
-}
-
-TYPE delta(TYPE S, TYPE delta_0, TYPE S_m) {
-    return delta_0 * (TYPE(1) - TYPE(S / S_m));
-}
-
-TYPE
-concentration_front(const std::function<TYPE(TYPE)> &f_flow_rate, TYPE h, INDEX_TYPE n, INDEX_TYPE multiplier = 100,
-                    TYPE result_0 = TYPE(0), const TYPE *concentration_front_till_previous = nullptr) {
-    if (concentration_front_till_previous != nullptr) {
-        TYPE almost_result = *concentration_front_till_previous;
-        return almost_result + h * f_flow_rate(TYPE(n - 1) * h);
-    }
-    TYPE result = result_0;
+FLOAT_TYPE
+concentration_front(const std::function<FLOAT_TYPE(FLOAT_TYPE)> &f_flow_rate, FLOAT_TYPE h, INDEX_TYPE n, INDEX_TYPE multiplier = 100,
+                    FLOAT_TYPE result_0 = FLOAT_TYPE(0)) {
+    FLOAT_TYPE result = result_0;
     n *= multiplier;
-    h /= TYPE(multiplier);
+    h /= FLOAT_TYPE(multiplier);
     for (INDEX_TYPE i = 0; i < n; i++) {
-        result += h * f_flow_rate(h * TYPE(i));
+        result += h * f_flow_rate(h * FLOAT_TYPE(i));
     }
     return result;
 }
 
 
-TYPE exact_flow_rate(TYPE t, TYPE s_m, TYPE delta_0) {
-    return exp((TYPE(-1) * delta_0 * t) / s_m);
+FLOAT_TYPE exact_flow_rate(FLOAT_TYPE t, FLOAT_TYPE s_m, FLOAT_TYPE delta_0) {
+//    return (TYPE(1) - delta_0 * t / s_m);
+    return exp(- delta_0 * t / s_m);
 }
 
 
+FLOAT_TYPE exact_concentration_front(FLOAT_TYPE t, FLOAT_TYPE s_m, FLOAT_TYPE delta_0) {
+//    if (fabs(delta_0) < eps) {
+//        return t;
+//    }
+//    const auto T = s_m / delta_0;
+////    return t - ((t * t) / 2 * T);
+//    return T * (FLOAT_TYPE(1) - exp(-t / T));
+    return t;
+}
+#else
+FLOAT_TYPE eps;
+FLOAT_TYPE one;
+FLOAT_TYPE zero;
+FLOAT_TYPE exponent_const;
+#endif
+
+
 int main(int argc, char **argv) {
+    std::cout << "Starting program!\n";
+#ifdef MPZ_T
+    try {
+        mpf_init2(eps, 64);
+        mpf_init2(one, 64);
+        mpf_init2(zero, 64);
+        mpf_init2(exponent_const, 64);
+        mpf_set_str(eps, "0.0001", 10);
+        mpf_set_str(one, "1", 10);
+        mpf_set_str(zero, "0", 10);
+        mpf_set_str(exponent_const, "2.718281828459045235360287471352", 10);
+    } catch (const std::exception& exception) {
+        std::cout << exception.what() << '\n';
+        return 1;
+    }
+    mp_exp_t mp_exp = 1;
+    std::cout << mpf_get_str(nullptr, &mp_exp, 10, 20, exponent_const) << "E" << mp_exp << '\n';
+#endif
     if (argc != 2) {
         std::cout << "Usage: porous_media [configuration_file.json]\n";
         return -1;
@@ -66,185 +106,243 @@ int main(int argc, char **argv) {
         std::filesystem::create_directory("results");
     }
 
+    std::cerr.precision(20);
+    std::cout.precision(20);
+
     INDEX_TYPE i, j;
-    INDEX_TYPE M, N;
-    TYPE h;
-    TYPE delta_0 = TYPE(1);
-    TYPE s_m = TYPE(0.5);
-    TYPE c_x_0 = TYPE(0);
-    TYPE c_0_t = TYPE(1);
-    TYPE s_x_0 = TYPE(0);
     std::cout << "Configuration: " << argv[1] << '\n';
     std::ifstream configuration_json(argv[1], std::ifstream::binary);
     Json::Reader reader;
     Json::Value root;
     reader.parse(configuration_json, root);
-    M = root.get("M", TYPE(0)).asInt64();
-    N = root.get("N", TYPE(0)).asInt64();
-    std::cout << std::setprecision(5);
+    const INDEX_TYPE M = root.get("M", (INDEX_TYPE) 0).asInt64();
+    const INDEX_TYPE N = root.get("N", (INDEX_TYPE) 0).asInt64();
 
-    {
-        h = root.get("h", TYPE(1)).as<TYPE>();
-        delta_0 = root.get("delta_0", TYPE(1)).as<TYPE>();
-        s_m = root.get("s_m", TYPE(1)).as<TYPE>();
-//        a = root.get("a", TYPE(0.5)).as<TYPE>();
-//        b = root.get("b", TYPE(1)).as<TYPE>();
-        c_x_0 = root.get("C_x_0", TYPE(0)).as<TYPE>();
-        c_0_t = root.get("C_0_t", TYPE(1)).as<TYPE>();
-        s_x_0 = root.get("S_x_0", TYPE(0)).as<TYPE>();
-    }
+
+#ifndef MPZ_T
+    const FLOAT_TYPE dx = (FLOAT_TYPE)root.get("dx", double(1)).as<double>();
+    const FLOAT_TYPE dt = (FLOAT_TYPE)root.get("dt", double(1)).as<double>();
+    const FLOAT_TYPE delta_0 = (FLOAT_TYPE)root.get("delta_0", double(1)).as<double>();
+    const FLOAT_TYPE s_m = (FLOAT_TYPE)root.get("s_m", double(1)).as<double>();
+    const FLOAT_TYPE c_x_0 = (FLOAT_TYPE)root.get("C_x_0", double(0)).as<double>();
+    const FLOAT_TYPE c_0_t = (FLOAT_TYPE)root.get("C_0_t", double(1)).as<double>();
+    const FLOAT_TYPE s_x_0 = (FLOAT_TYPE)root.get("S_x_0", double(0)).as<double>();
+    const FLOAT_TYPE a = (FLOAT_TYPE) root.get("a", double(1.)).as<double>();
+    const FLOAT_TYPE b = (FLOAT_TYPE) root.get("b", double(2.)).as<double>();
 
     std::cout << "Configuration:\n";
     out("M", M)
     out("N", N)
-    out("h", h)
+    out("dx", dx)
+    out("dt", dt)
+    out("s_m", s_m)
+    out("delta_0", delta_0)
     out("c_x_0", c_x_0)
     out("c_0_t", c_0_t)
     out("s_x_0", s_x_0)
     std::cout << '\n';
-    const std::function<TYPE(TYPE)> S_0 = [delta_0, s_m](TYPE t) { return s_0(t, delta_0, s_m); };
-    const std::function<TYPE(TYPE)> flow_rate = [delta_0, s_m](TYPE t) { return v(t, s_0, s_m, delta_0); };
-    const std::function<TYPE(TYPE)> Delta = [delta_0, s_m](TYPE S) { return delta(S, delta_0, s_m); };
+#else
+    const auto dx_string = root.get("dx", "0.001").as<std::string>();
+    const auto dt_string = root.get("dt", "0.0005").as<std::string>();
+    const auto delta_0_string = root.get("delta_0", "1").as<std::string>();
+    const auto s_m_string = root.get("s_m", "1").as<std::string>();
+    const auto c_x_0_string = root.get("C_x_0", "0").as<std::string>();
+    const auto c_0_t_string = root.get("C_0_t", "1").as<std::string>();
+    const auto s_x_0_string = root.get("S_x_0", "0").as<std::string>();
+
+    FLOAT_TYPE dx;
+    mpf_init2(dx, 64);
+    mpf_set_str(dx, dx_string.c_str(), 10);
+    FLOAT_TYPE dt;
+    mpf_init2(dt, 64);
+    mpf_set_str(dt, dt_string.c_str(), 10);
+    FLOAT_TYPE delta_0;
+    mpf_init2(delta_0, 64);
+    mpf_set_str(delta_0, delta_0_string.c_str(), 10);
+    FLOAT_TYPE s_m;
+    mpf_init2(s_m, 64);
+    mpf_set_str(s_m, s_m_string.c_str(), 10);
+    FLOAT_TYPE c_x_0;
+    mpf_init2(c_x_0, 64);
+    mpf_set_str(c_x_0, c_x_0_string.c_str(), 10);
+    FLOAT_TYPE c_0_t;
+    mpf_init2(c_0_t, 64);
+    mpf_set_str(c_0_t, c_0_t_string.c_str(), 10);
+    FLOAT_TYPE s_x_0;
+    mpf_init2(s_x_0, 64);
+    mpf_set_str(s_x_0, s_x_0_string.c_str(), 10);
+#endif
+
+    const auto S_0 = [delta_0, s_m, a, b](FLOAT_TYPE t) {
+#ifdef MPZ_T
+        FLOAT_TYPE delta;
+        FLOAT_TYPE exponent;
+        FLOAT_TYPE exponent_argument;
+        mpf_init2(delta, 64);
+        mpf_init2(exponent, 64);
+        mpf_init2(exponent_argument, 64);
+        mpf_mul(exponent_argument, delta_0, t);
+        mpf_div(exponent_argument, exponent_argument, s_m);
+        mpf_neg(exponent_argument, exponent_argument);
+
+        mpf_sub(delta, one, exponent);
+        mpf_mul(delta, delta, s_m);
+        return delta;
+#else
+        return (a/b - exp(-b * t) / b);
+#endif
+    };
+
+#ifndef MPZ_T
+    const std::function<FLOAT_TYPE(FLOAT_TYPE)> flow_rate = [S_0, s_m, a, b](FLOAT_TYPE t) {
+//        return 1;
+//        return v(t, s_0, s_m, delta_0);
+//        return (FLOAT_TYPE(1) - S_0(t) / (a / b));
+        return FLOAT_TYPE(0.5);
+    };
+    const std::function<FLOAT_TYPE(FLOAT_TYPE)> Delta = [delta_0, s_m, a, b](FLOAT_TYPE S) {
+//        return (FLOAT_TYPE(1) - FLOAT_TYPE(2) * S);
+//        return delta(S, delta_0, s_m);
+        return (a - b * S);
+    };
 
 
-    Eigen::SparseMatrix<TYPE> c(N, M);
-    Eigen::SparseMatrix<TYPE> s(N, M);
+    Eigen::SparseMatrix<FLOAT_TYPE> c(N, M);
+    Eigen::SparseMatrix<FLOAT_TYPE> s(N, M);
 
     for (i = 0; i < N; i++) {
+        // C(x, 0) = C|t=0
         c.coeffRef(i, 0) = c_x_0;
     }
 
     for (i = 0; i < N; i++) {
+        // S(x, 0) = S|t=0
         s.coeffRef(i, 0) = s_x_0;
     }
 
     for (j = 0; j < M; j++) {
+        // C(0, t) = C|x=0
         c.coeffRef(0, j) = c_0_t;
     }
 
-    std::vector<TYPE> concentration_front_at_t;
-    std::vector<TYPE> flow_rate_at_t;
+    std::vector<FLOAT_TYPE> concentration_front_at_t;
+    std::vector<FLOAT_TYPE> flow_rate_at_t;
 
     for (j = 0; j < M; j++) {
-        if (j == 0) { concentration_front_at_t.push_back(concentration_front(flow_rate, h, j)); }
-        else {
-            concentration_front_at_t.push_back(
-                    concentration_front(flow_rate, h, j, 100, 0, &concentration_front_at_t[j - 1]));
+        // Вычисление точек фронта концентрации
+        const auto concentration_front_at_t_j = concentration_front(flow_rate, dt, j);
+        concentration_front_at_t.push_back(concentration_front_at_t_j);
+        // Проверка с точным решением
+#ifdef CHECK_EXACT_VALUES
+        if (fabs(concentration_front_at_t_j - exact_concentration_front(FLOAT_TYPE(j) * dt, s_m, delta_0)) > eps) {
+            std::cerr << j << '\n';
+            std::cerr << concentration_front_at_t_j << " <> " << exact_concentration_front(FLOAT_TYPE(j) * dt, s_m, delta_0)
+                      << '\n';
+            throw std::runtime_error("Concentration front mismatch!");
         }
-//        std::cout << "t = " << TYPE(j) * h << " x(t) = " << concentration_fron_at_t[j] << '\n';
-        const auto flow_rate_at_time_j = flow_rate(TYPE(j) * h);
+#endif
+        // Вычисление точек скорости распространения суспензии
+        const auto flow_rate_at_time_j = flow_rate(FLOAT_TYPE(j) * dt);
         flow_rate_at_t.push_back(flow_rate_at_time_j);
-//        std::cout << "v(" << TYPE(j) * h << ") = " << flow_rate_at_time_j << '\n';
-        if (fabs(flow_rate_at_time_j - exact_flow_rate(TYPE(j) * h, s_m, delta_0)) > eps) {
+        // Сравнение
+#ifdef CHECK_EXACT_VALUES
+        if (fabs(flow_rate_at_time_j - exact_flow_rate(FLOAT_TYPE(j) * dt, s_m, delta_0)) > eps) {
+            std::cerr << "Flow rate differs with exact one!";
             throw std::runtime_error("Flow rate differs with exact one!");
         }
-
+#endif
     }
 
     // 'i' is for x measure, 'j' is for time measure
     for (j = 0; j < M - 1; j++) {
         const auto flow_rate_j = flow_rate_at_t[j];
         const auto concentration_front = concentration_front_at_t[j];
+        const auto concentration_front_next = concentration_front_at_t[j + 1];
         for (i = 0; i < N - 1; i++) {
-            const auto x_i = TYPE(i) * h;
-            if (concentration_front > x_i) {
-                const auto c_i_j = c.coeff(i, j);
+            const auto x_i = FLOAT_TYPE(i) * dx;
+            const auto x_i_next = x_i + dx;
+            const auto c_i_j = c.coeff(i, j);
+            const auto s_i_j = s.coeff(i, j);
+            const auto ds_dt = Delta(s_i_j) * c_i_j;
+            s.coeffRef(i, j + 1) = s_i_j + dt * ds_dt;
+            if (((concentration_front_next - x_i_next) > eps) && ((concentration_front - x_i) > eps)) {
                 const auto c_i_j_next = c.coeff(i, j + 1);
-                const auto s_i_j = s.coeff(i, j);
+                const auto dc_dt = (c_i_j_next - c_i_j) / dt;
+                const auto dc_dx = (-ds_dt - dc_dt) / flow_rate_j;
 
-                const auto c_i_j_div_flow_rate = c_i_j / flow_rate_j;
-                const auto c_i_j_next_div_flow_rate = c_i_j_next / flow_rate_j;
-                c.coeffRef(i + 1, j + 1) = c_i_j_next + c_i_j_div_flow_rate - c_i_j_next_div_flow_rate - c_i_j_div_flow_rate * Delta(s_i_j) * h;
-                s.coeffRef(i, j + 1) = s_i_j + h * Delta(s_i_j) * c_i_j;
+                const auto c_i_next_j_next = c_i_j_next + dx * dc_dx;
+                c.coeffRef(i + 1, j + 1) = c_i_next_j_next;
 
-#ifndef TESTING
-                if (c.coeff(i + 1, j) > c_0_t) {
-                    std::cerr << "C(" + std::to_string(x_i) + ", " + std::to_string(TYPE(j) * h) +
-                                 ") became more than c_0_t!\n";
-                    std::cerr << "C(i + 1, j + 1) = " << c.coeffRef(i + 1, j) << '\n';
-                    std::cerr << "C(i, j) = " << C_i_j << '\n';
-                    std::cerr << "C(i, j + 1) = " << C_i_j_next << '\n';
-                    std::cerr << "S(i, j) = " << S_i_j << '\n';
-                    std::cerr << "/\\(S) = " << delta_s << '\n';
+                if (fabs(((c_i_j_next - c_i_j) + (flow_rate_j * (c_i_next_j_next - c_i_j_next)) +
+                          (Delta(s_i_j) * c_i_j * dt))) >= std::max(dx, dt)) {
+                    std::cerr << "C(" << i + 1 << ", " << j + 1 << ") = " << c_i_next_j_next << '\n';
+                    std::cerr << "C(" << i << ", " << j << ") = " << c_i_j << '\n';
+                    std::cerr << "C(" << i << ", " << j + 1 << ") = " << c_i_j_next << '\n';
+                    std::cerr << "S(" << i << ", " << j << ") = " << s_i_j << '\n';
+                    std::cerr << "/\\(S) = " << Delta(s_i_j) << '\n';
                     std::cerr << "v(t) = " << flow_rate_j << '\n';
+                    std::cerr << "dc/dx = " << c_i_next_j_next - c_i_j_next << '\n';
+                    std::cerr << "dc/dt = " << c_i_j_next - c_i_j << '\n';
+                    std::cerr << "v * dc/dx = " << flow_rate_j * (c_i_next_j_next - c_i_j_next) << '\n';
+                    std::cerr << "ds/dt = " << Delta(s_i_j) * c_i_j * dt << '\n';
+
+                    std::cerr << "dc/dt + v*dc/dx + ds/dt = "
+                              << ((c_i_j_next - c_i_j) + (flow_rate_j * (c_i_next_j_next - c_i_j_next)) +
+                                  (Delta(s_i_j) * c_i_j * dt)) << " = 0?" << '\n';
 
                     std::cerr << "x = " << x_i << " Concentration front G " << concentration_front << '\n';
                     std::cerr << "i = " << i << " j = " << j << '\n';
-                    throw std::runtime_error("Calculation error!");
+                    exit(3);
                 }
-                if (c.coeff(i + 1, j + 1) < 0) {
-                    std::cerr << "C(" + std::to_string(x_i) + ", " + std::to_string(TYPE(j + 1) * h) +
-                                 ") became less than 0!\n";
-                    std::cerr << "C(i + 1, j + 1) = " << c.coeffRef(i + 1, j + 1) << '\n';
-                    std::cerr << "C(i, j) = " << C_i_j << '\n';
-                    std::cerr << "C(i, j + 1) = " << C_i_j_next << '\n';
-                    std::cerr << "S(i, j) = " << S_i_j << '\n';
-                    std::cerr << "/\\(S) = " << delta_s << '\n';
-                    std::cerr << "v(t) = " << flow_rate_j << '\n';
-
-                    std::cerr << x_i << ' ' << concentration_front << '\n';
-                    std::cerr << i << ' ' << j << '\n';
-                    throw std::runtime_error("Calculation error!");
-                }
-                if (s.coeff(i, j + 1) >= s_m) {
-                    std::cerr << "S(" + std::to_string(x_i) + ", " + std::to_string(TYPE(j + 1) * h) +
-                                 ") became more than s_m!\n";
-                    std::cerr << "C(i + 1, j + 1) = " << c.coeffRef(i + 1, j + 1) << '\n';
-                    std::cerr << "C(i, j) = " << C_i_j << '\n';
-                    std::cerr << "C(i, j + 1) = " << C_i_j_next << '\n';
-                    std::cerr << "S(i, j) = " << S_i_j << '\n';
-                    std::cerr << "/\\(S) = " << delta_s << '\n';
-                    std::cerr << "v(t) = " << flow_rate_j << '\n';
-
-                    std::cerr << x_i << ' ' << concentration_front << '\n';
-                    std::cerr << i << ' ' << j << '\n';
-                    throw std::runtime_error("Calculation error!");
-                }
-                if (isnan(c.coeffRef(i + 1, j + 1))) {
-                    std::cerr << "C(" + std::to_string(x_i + h) + ", " + std::to_string(TYPE(j + 1) * h) +
-                                 ") became nan!\n";
-                    std::cerr << "C(i + 1, j + 1) = " << c.coeffRef(i + 1, j + 1) << '\n';
-                    std::cerr << "C(i, j) = " << C_i_j << '\n';
-                    std::cerr << "C(i, j + 1) = " << C_i_j_next << '\n';
-                    std::cerr << "S(i, j) = " << S_i_j << '\n';
-                    std::cerr << "/\\(S) = " << delta_s << '\n';
-                    std::cerr << "v(t) = " << flow_rate_j << '\n';
-
-                    std::cerr << x_i << ' ' << concentration_front << '\n';
-                    std::cerr << i << ' ' << j << '\n';
-                    throw std::runtime_error("Calculation error!");
-                }
-#endif
             }
         }
     }
 
     std::cout << "Ready!" << std::endl;
     std::ofstream output("results/result.csv");
-    output << "h;\n" << h << "\n\n\n";
+    output.precision(20);
+    output << "dx;\n" << dx << "\n" << "dt;\n" << dt << "\n\n\n";
     output << "x;t;c(i, j);s;v;G;\n";
     std::cout << "Starting writing results!\n";
 
     for (i = 0; i < N; i++) {
-        const auto x = h * TYPE(i);
+        const auto x = dx * FLOAT_TYPE(i);
         for (j = 0; j < M; j++) {
-            const auto t = h * TYPE(j);
-            auto c_i_j = TYPE(0);
-            auto s_i_j = TYPE(0);
+            const auto t = dt * FLOAT_TYPE(j);
+            auto c_i_j = FLOAT_TYPE(0);
+            auto s_i_j = s.coeff(i, j);
+            auto dc_dt = FLOAT_TYPE(0);
+            auto dc_dx = FLOAT_TYPE(0);
+            auto ds_dt = FLOAT_TYPE(0);
             if (concentration_front_at_t[j] >= x) {
                 c_i_j = c.coeff(i, j);
-                s_i_j = s.coeff(i, j);
+                if (i < N - 1) {
+                    const auto c_i_next_j = c.coeff(i + 1, j);
+                    std::cout << c_i_next_j << " - " << c_i_j << '\n';
+                    dc_dx = (c_i_next_j - c_i_j) / dx;
+                }
+                if (j < M - 1) {
+                    const auto c_i_j_next = c.coeff(i, j + 1);
+                    dc_dt = (c_i_j_next - c_i_j) / dt;
+                    const auto s_i_j_next = s.coeff(i, j + 1);
+                    ds_dt = (s_i_j_next - s_i_j) / dt;
+                }
             }
             output << x << ';' << t << ';'
                    //<< i << ';' << j << ';'
                    << c_i_j << ';'
                    << s_i_j << ';'
                    << flow_rate_at_t[j] << ';'
-                   << concentration_front_at_t[j] << ';' << '\n';
+                   << concentration_front_at_t[j] << ';'
+//                   << dc_dx << ';'
+//                   << dc_dt << ';'
+//                   << ds_dt << ';'
+                   << '\n';
         }
     }
 
     std::cout << "Saved!\n";
     if (output.is_open()) { output.close(); }
+#endif
     return 0;
 }
